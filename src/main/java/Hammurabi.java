5a16e37a152a52a02cae4b996a71e46598710f6e
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-03-13 18:12:13 -0400

    revised

--- src/main/java/Hammurabi.java
+++ src/main/java/Hammurabi.java
@@ -1,100 +1,84 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Hammurabi {
     Random rand = new Random();
     static Scanner scanner = new Scanner(System.in);
-    private int population;
-    private int grain; //bushels of grain in storage
-    private int land;
-    int valueBushelsPerAcre = 19; // land value is 19 bushels/acre
-    private int year;
+    private int population=100;
+    private int grain=2800; //bushels of grain in storage
+    private int land=1000;
+    private int valueBushelsPerAcre = 19; // land value is 19 bushels/acre
+    private int year=1;
     private int acresPlanted;
     private int plagueDeaths;
     private int immigrants;
     private int starved;
     private int harvest;
-    private int acres;
-    private int landValue;
     private int grainEatenByRats;
+    private int starvedTotal;
 
     public static void main(String[] args) {
         new Hammurabi().playGame();
     }
 
-
-    private  void printSummary() {
-        System.out.println(" You are in year" + year + " of your ten year rule.");
-        if (plagueDeaths > 0) {
-            System.out.println(" A horrible plague has taken" + plagueDeaths + " lives.");
-        }
-        System.out.println(" In the previous year" + starved + " people starved to death.");
-        System.out.println("In the previous year" + immigrants + "people have entered our kingdom.");
-        System.out.println(" The total population is now" + population);
-        System.out.println(" We have harvested" + harvest + " bushels of grain");
-        System.out.println(" The kingdom owns" + acres + " acres of land");
-        System.out.println(" Land is now priced at" + landValue + "bushels per acre.");
-        if (grainEatenByRats > 0) {
-            System.out.println(" O  Hammurabi, rats have destroyed" + grainEatenByRats + " bushels! We only have " + grain + " remaining in storage.");
-        } else {
-            System.out.println(" The remaining bushels of grain in storage is" + grain);
-        }
-    }
     void playGame() {
-        int population = 100;
-        int bushelsOfGrain = 2800; //bushels of grain in storage
-        int land = 1000;
-        int valueBushelsPerAcre = 19; // land value is 19 bushels/acre
-        int years = 1;
-        int bought, acresPlanted, feed, plague, starved, immig, harvest, rats;
-        while (years <= 10) {
+        int feed, bought, sell;
+        while (year <= 10) {
             printSummary();
             bought = askHowManyAcresToBuy(valueBushelsPerAcre, grain);
-            if (bought > 0)
+            if (bought > 0) {
                 land += bought;
-            else
-                land -= askHowManyAcresToSell(valueBushelsPerAcre, grain);
+                grain -=bought*valueBushelsPerAcre;
+            } else {
+                sell = askHowManyAcresToSell(grain);
+                land -=sell;
+                grain =grain +(sell*valueBushelsPerAcre);
+            }
             feed = askHowMuchGrainToFeedPeople(grain);
             acresPlanted = askHowManyAcresToPlant(land, population, grain);
-            plague = plagueDeaths(population);
+            plagueDeaths = plagueDeaths(population);
             starved = starvationDeaths(population, feed);
-            immig = immigrants(population, land, grain);
+            starvedTotal+=starved;
+            immigrants = immigrants(population, land, grain);
             harvest = harvest(acresPlanted);
-            rats = grainEatenByRats(grain);
+            grainEatenByRats = grainEatenByRats(grain);
             valueBushelsPerAcre = newCostOfLand();
-            population = population - plague - starved + immig;
-            grain = grain - rats - feed + harvest;
-            years++;
+            population = population - plagueDeaths - starved + immigrants;
+            grain = grain - grainEatenByRats - feed + harvest;
+            year++;
         }
+        finalSummary();
     }
 
-    public static int askHowManyAcresToBuy (int price, int bushels) {
+    public static int askHowManyAcresToBuy(int price, int bushels) {
         //Scanner scan1 = new Scanner(System.in);
-        System.out.println("How many acres would you like to buy?");
+        //System.out.println("How many acres would you like to buy?");
         //Buying: Grain is paying for the purchase.
-        int numberOfAcresToBuy = scanner.nextInt();
+        int numberOfAcresToBuy = getNumber("How many acres would you like to buy?");
         do {
             if (numberOfAcresToBuy < 0) {
-                System.out.println("You can't use a negative number.");
+                System.out.println("O Hammurabi, but that is not a positive number!");
+                numberOfAcresToBuy = getNumber("How many acres would you like to buy?");
             } else if (numberOfAcresToBuy > 0 && bushels < price * numberOfAcresToBuy) {
-                System.out.println("You do not have enough to purchase.");
+                System.out.println("O Hammurabi, but you only have " + bushels + " bushels of grain!");
+                numberOfAcresToBuy = getNumber("How many acres would you like to buy?");
             } else if (numberOfAcresToBuy >= 0 && bushels > price * numberOfAcresToBuy) {
                 return numberOfAcresToBuy;
-
                 //bushels = bushels - (valueBushelsPerAcre * numberOfAcresToBuy);
-
             }
         } while (true);
     }
 
     public static int askHowManyAcresToSell(int acresOwned) {
         //Ask how many to sell.
-        System.out.println("How many acres do you want to sell?");
+        //System.out.println("How many acres do you want to sell?");
         //Scanner sellAnswer = new Scanner(System.in);
-        int numberOfAcresToSell = scanner.nextInt();
+        int numberOfAcresToSell = getNumber("How many acres do you want to sell?");
         do {
             if (numberOfAcresToSell > acresOwned) {
-                System.out.println("No can do.");
+                System.out.println("O Hammurabi, but you only have " + acresOwned + " acres of land!");
+                numberOfAcresToSell = getNumber("How many acres do you want to sell?");
             } else {
                 return numberOfAcresToSell;
             }
@@ -104,12 +88,12 @@ public class Hammurabi {
 
     public static int askHowMuchGrainToFeedPeople(int bushels) {
         int grainToFeed;
-        System.out.println("How much grain do you want to feed people?");
+        //System.out.println("How much grain do you want to feed people?");
         //Scanner grainAmount = new Scanner(System.in);
-        grainToFeed = scanner.nextInt();
         do {
+            grainToFeed = getNumber("How much grain do you want to feed people?");
             if (grainToFeed > bushels) {
-                System.out.println("No can do.");
+                System.out.println("O Hammurabi, but you only have " + bushels + " bushels of grain!");
             } else {
                 return grainToFeed;
             }
@@ -117,73 +101,89 @@ public class Hammurabi {
         //You can't sell more than you have.
     }
 
+    public int plagueDeaths(int population) {
+        return randomChance(15) ? population / 2 : 0;
+    }
+
+    public int newCostOfLand() {
+        return rand.nextInt(7) + 17; //bound is exclusive
+    }
+
+    public int harvest(int acresPlantedWithSeeds) {
+        return (rand.nextInt(6) + 1) * acresPlantedWithSeeds;
+    }
+
+    public int grainEatenByRats(int bushels) {
+        return randomChance(40)
+                ? (rand.nextInt(21) + 10) * bushels / 100 //bound is exclusive
+                : 0;
+    }
+
+    public boolean randomChance(double chanceOfOccurrence) {
+        return rand.nextDouble() < (chanceOfOccurrence / 100);
+    }
+
+    static int getNumber(String message) {
+        while (true) {
+            System.out.print(message);
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("\"" + scanner.next() + "\" isn't a number!");
+            }
+        }
+    }
+
+    private  void printSummary() {
+        System.out.println("You are in year" + year + " of your ten year rule.");
+        if (plagueDeaths > 0) {
+            System.out.println("A horrible plague has taken" + plagueDeaths + " lives.");
+        }
+        System.out.println("In the previous year " + starved + " people starved to death.");
+        System.out.println("In the previous year " + immigrants + "people have entered our kingdom.");
+        System.out.println("The total population is now " + population);
+        System.out.println("We have harvested " + harvest + " bushels of grain");
+        System.out.println("The kingdom owns " + land + " acres of land");
+        System.out.println("Land is now priced at " + valueBushelsPerAcre + "bushels per acre.");
+        if (grainEatenByRats > 0) {
+            System.out.println(" O  Hammurabi, rats have destroyed " + grainEatenByRats + " bushels! We only have " + grain + " remaining in storage.");
+        } else {
+            System.out.println(" The remaining bushels of grain in storage is " + grain);
+        }
+    }
+
     public static int askHowManyAcresToPlant(int acresOwned, int population, int bushels) {
-        String howManyAcresToPlantQuestion = "How many acres do you wish to plant?";
-        int acresToPlant = 0;
+        int acresToPlant=0;
         boolean validAcreage = false;
         while (!validAcreage) {
-
+            acresToPlant=getNumber("How many acres do you wish to plant?");
             if (acresToPlant > acresOwned) {
-                System.out.println(" O Hammurabi, but you only have " + acresOwned + " acres for which to plant!");
+                System.out.println("O Hammurabi, but you only have " + acresOwned + " acres for which to plant!");
             } else if (acresToPlant > 10 * population) {
-                System.out.println(" O Hammurabi, but you only have " + acresOwned + " acres for which to increase your grain!");
+                System.out.println("O Hammurabi, but you only have " + acresOwned + " acres for which to increase your grain!");
             } else if (acresToPlant < 2 * bushels) {
-                System.out.println(" O  Hammurabi, but you only have " + bushels + " bushels for which to plant");
+                System.out.println("O  Hammurabi, but you only have " + bushels + " bushels for which to plant!");
                 System.out.println(acresToPlant + " acres you wish to plant? We don't have the resources");
             } else validAcreage = true;
-
-//        } acresPlanted = acresToPlant / 2;
-//         grain = grain - acresToPlant;
-//        System.out.println( ", we now have " + grain
-//                + " bushels of grain in storage.");;
-//        return acresPlanted;
         }
         return acresToPlant;
     }
 
-    public int plagueDeaths (int population) {
-        return randomChance(15) ? population/2 : 0;
-    }
-
-    public int newCostOfLand(){
-        return rand.nextInt(7)+17; //bound is exclusive
-    }
-
-    public int harvest(int acresPlantedWithSeeds){
-        return (rand.nextInt(6)+1)*acresPlantedWithSeeds;
-    }
-
-    public int grainEatenByRats(int bushels){
-        return randomChance(40)
-                ? (rand.nextInt(21)+10)*bushels / 100 //bound is exclusive
-                :0;
-    }
-
-    public boolean randomChance(double chanceOfOccurrence){
-        return rand.nextDouble()<(chanceOfOccurrence/100);
-    }
-
-    
-    public void finalSummary(int acresOwned) {
-
+    public void finalSummary() {
         if (starved > (45 *  population) / 100) {
-            System.out.println( " Hammurabi, \n" + "you have starved " + starved + " of your subjects in your final year./n" + " Your time is over!");
+            System.out.println( "Hammurabi, \n" + "you have starved " + starvedTotal + " of your subjects in your final year./n" + " Your time is over!");
         }
-
-        if ( population * 20 < acresOwned) {
-            acresOwned = population * 20;
+        if ( population * 20 < land) {
+            land = population * 20;
         }
-
-        if (acresOwned < 700) {
-            System.out.println(" Ah hammurabi!!\n" + "you have put in much effort\n" +" but have only done an OKAY job..");
-        } else if (acresOwned < 900) {
-            System.out.println(" Ah hammurabi!!\n" + "you have put in much effort\n" + " And have done a MAGNIFICENT JOB!!");
+        if (land < 700) {
+            System.out.println("Ah hammurabi!!\n" + "you have put in much effort\n" +" but have only done an OKAY job..");
+        } else if (land < 900) {
+            System.out.println("Ah hammurabi!!\n" + "you have put in much effort\n" + " And have done a MAGNIFICENT JOB!!");
         }
-
         else {
-            System.out.println( " Congratulations!! You have done well by your people\n and will be praised for many years to come!!!");
+            System.out.println( "Congratulations!! You have done well by your people\n and will be praised for many years to come!!!");
         }
-
     }
 }
 
--- src/test/java/HammurabiTest.java
+++ src/test/java/HammurabiTest.java
@@ -19,30 +19,30 @@ public class HammurabiTest {
         ham = new Hammurabi();
     }
 
-//    @Test
-//    public final void testPlagueDeaths1() {
-//        int number_of_plagues = 0;
-//        for (int i = 0; i < 10000; i++) {
-//            int deaths = ham.plagueDeaths(100);
-//            if (deaths > 0) {
-//                number_of_plagues += 1;
-//            }
-//        }
-//        int percentPlagues = number_of_plagues / 100;
-//        assertTrue("Number of plagues is about " + percentPlagues + ", not about 15%.",
-//                   about(1500, number_of_plagues));
-//    }
-//
-//    @Test
-//    public final void testPlagueDeaths2() {
-//        int deaths = 0;
-//        for (int i = 0; i < 10000; i++) {
-//            deaths = ham.plagueDeaths(100);
-//            if (deaths > 0) break;
-//        }
-//        assertEquals("In a plague, " + deaths + "% of your people die, not 50%.",
-//                     50, deaths);
-//    }
+    @Test
+    public final void testPlagueDeaths1() {
+        int number_of_plagues = 0;
+        for (int i = 0; i < 10000; i++) {
+            int deaths = ham.plagueDeaths(100);
+            if (deaths > 0) {
+                number_of_plagues += 1;
+            }
+        }
+        int percentPlagues = number_of_plagues / 100;
+        assertTrue("Number of plagues is about " + percentPlagues + ", not about 15%.",
+                   about(1500, number_of_plagues));
+    }
+
+    @Test
+    public final void testPlagueDeaths2() {
+        int deaths = 0;
+        for (int i = 0; i < 10000; i++) {
+            deaths = ham.plagueDeaths(100);
+            if (deaths > 0) break;
+        }
+        assertEquals("In a plague, " + deaths + "% of your people die, not 50%.",
+                     50, deaths);
+    }
 //
 //    @Test
 //    public final void testStarvationDeaths() {
@@ -66,80 +66,60 @@ public class HammurabiTest {
 //        assertEquals("Wrong number of immigrants.", 25, imm);
 //    }
 
-//    @Test
-//    public final void testHarvest() {
-//        int[] yield = new int[7];
-//        for (int i = 0; i < 1000; i++) {
-//            int harvest = ham.harvest(1);
-//            assertTrue("Illegal harvest per acre: " + harvest, harvest > 0 && harvest <= 6);
-//            yield[harvest] += 1;
-//        }
-//        for (int j = 1; j <= 6; j++) {
-//            assertTrue("You never have a yield of " + j + " bushels per acre.", yield[j] > 0);
-//        }
-//    }
-
-//    @Test
-//    public final void testGrainEatenByRats1() {
-//        int infestations = 0;
-//        for (int i = 0; i < 1000; i++) {
-//            int eaten = ham.grainEatenByRats(100);
-//            if (eaten > 0) {
-//                infestations += 1;
-//            }
-//        }
-//        int percentInfestations = infestations / 100;
-//        assertTrue("Number of rat infestations is about " + percentInfestations +
-//                   ", not about 40%.", about(400, infestations));
-//    }
-//
-//    @Test
-//    public final void testGrainEatenByRats2() {
-//        int percent = 0;
-//        int[] counts = new int[31];
-//        for (int i = 0; i < 10000; i++) {
-//            percent = ham.grainEatenByRats(100);
-//            if (percent == 0) continue;
-//            counts[percent] += 1;
-//            assertTrue("Rats ate " + percent + "% of your grain, not 10% to 30%.",
-//                       percent >= 10 && percent <= 30);
-//        }
-//        for (int j = 11; j < 30; j++) {
-//            assertTrue("Rats never ate " + j + "% of your grain.", counts[j] > 0);
-//        }
-//    }
-//
-//    @Test
-//    public final void testNewCostOfLand() {
-//        int[] cost = new int[24];
-//        for (int i = 0; i < 1000; i++) {
-//            int price = ham.newCostOfLand();
-//            assertTrue("Illegal cost of land: " + price, price >= 17 && price <= 23);
-//            cost[price] += 1;
-//        }
-//        for (int j = 17; j <= 23; j++) {
-//            assertTrue("You never have a land cost of " + j + " bushels per acre.", cost[j] > 0);
-//        }
-//    }
-
     @Test
-    public final void acresToPlantTest(){
-        //Given
-//        int population = 100;
-//        int bushels = 1;
-//        int acresOwned = 35;
-        int acresToPlant = 10;
-
-
-        //When
-        int actual = Hammurabi.askHowManyAcresToPlant(20, 200, 35);
-
-
-        //Then
-        Assert.assertEquals(acresToPlant, actual);
-
-
+    public final void testHarvest() {
+        int[] yield = new int[7];
+        for (int i = 0; i < 1000; i++) {
+            int harvest = ham.harvest(1);
+            assertTrue("Illegal harvest per acre: " + harvest, harvest > 0 && harvest <= 6);
+            yield[harvest] += 1;
+        }
+        for (int j = 1; j <= 6; j++) {
+            assertTrue("You never have a yield of " + j + " bushels per acre.", yield[j] > 0);
+        }
     }
 
+    @Test
+    public final void testGrainEatenByRats1() {
+        int infestations = 0;
+        for (int i = 0; i < 1000; i++) {
+            int eaten = ham.grainEatenByRats(100);
+            if (eaten > 0) {
+                infestations += 1;
+            }
+        }
+        int percentInfestations = infestations / 100;
+        assertTrue("Number of rat infestations is about " + percentInfestations +
+                   ", not about 40%.", about(400, infestations));
+    }
+
+    @Test
+    public final void testGrainEatenByRats2() {
+        int percent = 0;
+        int[] counts = new int[31];
+        for (int i = 0; i < 10000; i++) {
+            percent = ham.grainEatenByRats(100);
+            if (percent == 0) continue;
+            counts[percent] += 1;
+            assertTrue("Rats ate " + percent + "% of your grain, not 10% to 30%.",
+                       percent >= 10 && percent <= 30);
+        }
+        for (int j = 11; j < 30; j++) {
+            assertTrue("Rats never ate " + j + "% of your grain.", counts[j] > 0);
+        }
+    }
+
+    @Test
+    public final void testNewCostOfLand() {
+        int[] cost = new int[24];
+        for (int i = 0; i < 1000; i++) {
+            int price = ham.newCostOfLand();
+            assertTrue("Illegal cost of land: " + price, price >= 17 && price <= 23);
+            cost[price] += 1;
+        }
+        for (int j = 17; j <= 23; j++) {
+            assertTrue("You never have a land cost of " + j + " bushels per acre.", cost[j] > 0);
+        }
+    }
 }
 
