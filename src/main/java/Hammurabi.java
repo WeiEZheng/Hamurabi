96da09ed030d0810004c5160373517bc91963544
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-03-13 20:00:49 -0400

    added my code

--- src/main/java/Hammurabi.java
+++ src/main/java/Hammurabi.java
@@ -185,7 +185,20 @@ public class Hammurabi {
             System.out.println( "Congratulations!! You have done well by your people\n and will be praised for many years to come!!!");
         }
     }
-}
 
+    public static int immigrants(int population,int acresOwned,int grainInStorage){
+        return (20 * acresOwned + grainInStorage)/(100 * population) +1;
+
+    }
+
+    public static int starvationDeaths(int population, int bushelsFedtoPeople){
+        if(population *20 <= bushelsFedtoPeople) return 0;
+        else return population - (bushelsFedtoPeople/20);
+    }
+
+    public static boolean uprising(int population, int howManyPeopleSarved){
+        return (((double)howManyPeopleSarved/population) >= .45);
+    }
+}
 
 
